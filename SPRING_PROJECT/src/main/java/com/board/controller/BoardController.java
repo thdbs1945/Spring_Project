590f0d77e58ad073511573292df07922a69998a6
TA9 <TA9@DESKTOP-UDO7TDQ>
2020-11-16 11:12:31 +0900

    commit 2020-11-16 update

--- SPRING_PROJECT/src/main/java/com/board/controller/BoardController.java
+++ SPRING_PROJECT/src/main/java/com/board/controller/BoardController.java
@@ -46,6 +46,16 @@ public class BoardController {
 		model.addAttribute("list",service.list());
 		return "board/list";
 	}
+	
+	// 게시판 조회
+		@RequestMapping(value = "/readView", method = RequestMethod.GET)
+		public String read(BoardVO boardVO, Model model) throws Exception{
+			logger.info("read");
+			
+			model.addAttribute("read", service.read(boardVO.getBno()));
+			
+			return "board/readView";
+		}
 
 	
 	// 게시판 수정뷰
